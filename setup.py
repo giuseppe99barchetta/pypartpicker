189887e995511a9bfac0983340a0f49ea787203d
Giuseppe Barchetta <55151669+giuseppe99barchetta@users.noreply.github.com>
2024-06-13 11:46:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as readme:
     long_description = readme.read()
 
 setup(
-    name="pypartpicker",
-    version="1.9.5",
+    name="pypartpicker2",
+    version="0.1.4",
     description="A package that scrapes pcpartpicker.com and returns the results as objects.",
     packages=["pypartpicker"],
-    url="https://github.com/thefakequake/pypartpicker",
+    url="https://github.com/giuseppe99barchetta/pypartpicker",
     keywords=["pcpartpicker", "scraper", "list", "beautifulsoup", "pc", "parts"],
     install_requires=["bs4", "requests"],
     zip_safe=False,
-    download_url="https://github.com/thefakequake/pypartpicker/archive/refs/tags/v1.9.5.tar.gz",
+    download_url="https://github.com/giuseppe99barchetta/pypartpicker/archive/refs/tags/0.1.4.tar.gz",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
